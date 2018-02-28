34f9fcac75ad9586de2a7cebee1b2a02edbfd26e
ha <csh.shop@gmail.com>
2018-02-28 10:39:28 +0100

    * moved tab chooser to the rigth side

--- modules/web/src/com/company/workshop/web/orderinputobject/OrderInputObjectEdit.java
+++ modules/web/src/com/company/workshop/web/orderinputobject/OrderInputObjectEdit.java
@@ -1,7 +1,31 @@
 package com.company.workshop.web.orderinputobject;
 
-import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.workshop.entity.OrderInputObject;
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.TabSheet;
+
+import javax.inject.Inject;
 
 public class OrderInputObjectEdit extends AbstractEditor<OrderInputObject> {
+
+    @Inject
+    TabSheet mainTabSheet;
+
+
+    public void showMainTab() {
+        mainTabSheet.setSelectedTab("orderLocals");
+    }
+
+    public void showPartsTab() {
+            mainTabSheet.setSelectedTab("parts");
+    }
+
+    public void showClientsTab() {
+                mainTabSheet.setSelectedTab("clients");
+
+    }
+
+    public void showMechanincsTab() {
+   mainTabSheet.setSelectedTab("mechanincs");
+    }
 }
